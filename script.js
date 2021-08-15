835b65a856ac7d6ed28e205e36da8de82bb51859
faheemzama <faheemzama786@gmail.com>
2021-08-15 11:21:38 +0530

    DOM

---
+++ script.js
@@ -0,0 +1,11 @@
+document.title = "Learning DOM !!";
+console.log(document.title);
+
+const heading = document.getElementById("acess");
+console.log(heading);
+
+const text = document.getElementsByTagName("p");
+console.log(text);
+
+const select = document.querySelector("h3");
+console.log(select);
